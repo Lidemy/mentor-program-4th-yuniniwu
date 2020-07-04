b162b2bbc61f9d0fa1aa1000ec654e1d1b49b9e8
Yuni <20063249+yuniniwu@users.noreply.github.com>
2020-07-04 23:29:50 +0800

    week2 homeworks

--- homeworks/week2/hw3.js
+++ homeworks/week2/hw3.js
@@ -1,5 +1,23 @@
 function reverse(str) {
-  
+  // --------------------use while loop--------------------
+  // let n = str.length - 1;
+  // let i = 0;
+  // let newStr = "";
+  // while (i <= n) {
+  //   newStr += str[n - i];
+  //   i++;
+  // }
+  // console.log(newStr);
+  // --------------------use for loop--------------------
+  let n = str.length;
+  let newStr = "";
+  for (let i = 1; i <= n; i++) {
+    newStr += str[n - i];
+  }
+  console.log(newStr);
 }
 
-reverse('hello');
+reverse("hello");
+reverse("yoyoyo");
+reverse("1abc2");
+reverse("1,2,3,2,1");
--- homeworks/week2/hw5.js
+++ homeworks/week2/hw5.js
@@ -1,10 +1,25 @@
 function join(arr, concatStr) {
-  
+  let newStr = "";
+  for (let i = 0; i < arr.length - 1; i++) {
+    newStr += arr[i];
+    newStr += concatStr;
+  }
+  newStr += arr[arr.length - 1];
+  return newStr;
 }
 
 function repeat(str, times) {
-  
+  let newStr = "";
+  for (let i = 0; i < times; i++) {
+    newStr += str;
+  }
+  return newStr;
 }
 
-console.log(join(['a'], '!'));
-console.log(repeat('a', 5));
+console.log(join([1, 2, 3], ""));
+console.log(join(["a", "b", "c"], "!"));
+console.log(join(["a", 1, "b", 2, "c", 3], ","));
+console.log(join(["aaa", "bb", "c", "dddd"], ",,"));
+
+console.log(repeat("a", 5));
+console.log(repeat("yoyo", 2));
