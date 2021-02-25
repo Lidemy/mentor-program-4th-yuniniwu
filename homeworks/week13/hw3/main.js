074df350ee19ec79e50d5044b928952a8d3b6a16
Yuni <20063249+yuniniwu@users.noreply.github.com>
2021-02-26 00:09:37 +0800

    "try... catch" arrange adjusted

--- homeworks/week13/hw3/main.js
+++ homeworks/week13/hw3/main.js
@@ -16,7 +16,19 @@ const streamTemplate = `
         </div>
         `
 
-getTopGames()
+displayTopGames()
+
+async function displayTopGames() {
+  const data = await getTopGames()
+  const games = data.top
+  for (let i = 0; i < 5; i++) {
+    const element = document.createElement('li')
+    element.innerText = games[i].game.name
+    document.querySelector('.navbar').appendChild(element)
+  }
+  // 顯示第一個遊戲的實況
+  changeGame(games[0].game.name)
+}
 
 // 點了會顯示第二個遊戲的實況
 document.querySelector(".navbar").addEventListener("click", (e) => {
@@ -26,22 +38,15 @@ document.querySelector(".navbar").addEventListener("click", (e) => {
 
 // 拿前五個熱門遊戲放到 navbar
 async function getTopGames () {
-  const response = await fetch(API_URL + "/games/top?limit=5", {
-    headers: new Headers({
-      'Client-ID': CLIENT_ID,
-      'Accept': 'application / vnd.twitchtv.v5 + json'
-    })
-  })
-  const data  = await response.json()
-  const games = data.top
   try {
-    for (let i = 0; i < 5; i++) {
-      const element = document.createElement('li')
-      element.innerText = games[i].game.name
-      document.querySelector('.navbar').appendChild(element)
-    }
-    // 顯示第一個遊戲的實況
-    changeGame(games[0].game.name)
+    const response = await fetch(API_URL + "/games/top?limit=5", {
+      headers: new Headers({
+        'Client-ID': CLIENT_ID,
+        'Accept': 'application / vnd.twitchtv.v5 + json'
+      })
+    })
+    const data  = await response.json()
+    return data
   } catch (err) {
     alert(errMsg)
     console.log(err)
@@ -50,27 +55,27 @@ async function getTopGames () {
 
 // 取得特定遊戲的前 20 個熱門 live stream
 async function getStreams(gameName) {
-  const response = await fetch(API_URL + "/streams/?game=" + encodeURIComponent(gameName), {
-    headers: new Headers({
-      'Client-ID': CLIENT_ID,
-      'Accept': 'application / vnd.twitchtv.v5 + json'
+  try {
+    const response = await fetch(API_URL + "/streams/?game=" + encodeURIComponent(gameName), {
+      headers: new Headers({
+        'Client-ID': CLIENT_ID,
+        'Accept': 'application / vnd.twitchtv.v5 + json'
+      })
     })
-  })
-  const data = await response.json()
-  return data
+    const data = await response.json()
+    return data
+  } catch (err) {
+    alert(errMsg)
+    console.log(err)
+  }
 }
 
 async function changeGame(gameName) {
   document.querySelector("h1").innerText = gameName
   document.querySelector(".streams").innerHTML = ""
-  try {
-    const data = await getStreams(gameName)
-    for (let i = 0; i < 20; i++) {
-      appendStream(data.streams[i])
-    }
-  } catch (err) {
-    alert(errMsg)
-    console.log(err)
+  const data = await getStreams(gameName)
+  for (let i = 0; i < 20; i++) {
+    appendStream(data.streams[i])
   }
 }
 
